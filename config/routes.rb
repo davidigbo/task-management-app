76248af5aaa631277e2d9593cc97db79a2841ea8
david30-maker <davidokpah396@gmail.com>
2024-05-17 13:45:07 +0100

    add routes for task controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  namespace :api do
+    namespace :v1 do
+      resources :tasks
+    end
+  end
 end
