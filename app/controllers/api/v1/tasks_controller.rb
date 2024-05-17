6d4e8c6d68ab8f01773815be6ebc974c414d0622
david30-maker <davidokpah396@gmail.com>
2024-05-17 13:42:24 +0100

    add controller CRUD actions

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::TasksController < ApplicationController
+    before_action :set_task, only: [:show, :update, :destroy]
+
+    def index
+      tasks = current_user.tasks
+      render json: tasks
+    end
+
+    def show 
+      render json:@task
+    end
+
+    def create 
+      task = current_user.tasks.new(task_params)
+      if task.save 
+        render json: task
+      else
+        render json: task.errors, status: :unprocessable_entity
+      end
+    end  
+
+    def update
+      if @task.update(task_params)
+        render json: @task
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end 
+    end
+
+    def destroy 
+      @task.destroy 
+      render json: {message: "Task deleted"}
+    end
+
+    private
+
+    def set_task 
+      @task = current_user.tasks.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :description, :status, :completed)
+    end
+end
