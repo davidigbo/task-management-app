944a3e7853af4a44211fd08d42e0847a5c30ce0b
david30-maker <davidokpah396@gmail.com>
2024-05-17 11:54:12 +0100

    add model association between user and task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :status, inclusion: { in: %w[not_started in_progress completed] }
+  validates :completed, inclusion: { in: [true, false] }
+
+  before_save :update_completed
+
+  private
+
+  def update_completed
+    self.completed = true if status == 'completed'
+  end
 end
