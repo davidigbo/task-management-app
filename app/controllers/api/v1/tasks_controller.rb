256109670bb0e38478d705b927d1a5f2c08160c3
david30-maker <davidokpah396@gmail.com>
2024-05-20 06:00:22 +0100

    create connnection.rb file and update action cable in task_controller file

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::TasksController < ApplicationController
     def create 
       task = current_user.tasks.new(task_params)
       if task.save 
-        ActionCable.server.broadcast "task_channel", "Data created!"
+        ActionCable.server.broadcast "task_channel", task: task 
         render json: task, status: :created
       else
         render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
